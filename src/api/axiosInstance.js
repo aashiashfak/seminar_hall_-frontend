748c10686491964833f37be99a948b931a13431f
aashiashfak <aashiashfak@gmail.com>
2024-07-23 17:59:34 +0530

    added hosted backend axiosinstance

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -3,7 +3,7 @@ import axios from "axios";
 axios.defaults.withCredentials = true;
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8000/",
+  baseURL: "https://seminar-hall-backend.vercel.app/",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
